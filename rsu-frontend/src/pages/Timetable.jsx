6794bd633cf8b9ab85d507039d7ef43c33348fab
ksk155 <kaungsetkyaw1.6@gmail.com>
2025-11-28 07:49:47 +0700

    D

--- rsu-frontend/src/pages/Timetable.jsx
+++ rsu-frontend/src/pages/Timetable.jsx
@@ -202,6 +202,7 @@ export default function Timetable() {
           justifyContent: "space-between",
           alignItems: "center",
           marginTop: "12px",
+          pointerEvents: "auto",
         }}
       >
         {/* LEFT SIDE */}
